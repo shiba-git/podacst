dbd3fa202eba50cbcb97524528c6ae5d589ec4f7
shiba-git <shiba.webbusiness@gmail.com>
2016-06-01 01:02:58 +0900

    Added dashboard for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
-
+	before_action :find_user, only: [:show, :dashboard]
+	before_action :find_episode, only: [:show, :dashboard]
+	
 	def index 
 		@users = User.all.order("created_at DESC")
 	end
 
 	def show
-		@user = User.find(params[:id])
-		@episodes = Episode.where(user_id: @user).order("created_at DESC")
 	end
+
+	def dashboard
+	end
+
+	private
+
+		def find_user
+			if params[:id].nil?
+				@user = current_user
+			else
+				@user = User.find(params[:id])
+			end
+		end
+
+		def find_episode
+			@episodes = Episode.where(user_id: @user).order("created_at DESC")
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
 
   resources :users, only: [:index, :show] do 
     resources :episodes
   end
+
+  authenticated :user do 
+  	root 'users#dashboard', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 end
