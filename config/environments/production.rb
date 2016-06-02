bc5d2fee118b5dc34e90cb294ab16511541f448a
shiba-git <shiba.webbusiness@gmail.com>
2016-06-02 16:09:22 +0900

    Add hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #set paperclip to Updated to Amazon S3 by default
-  
+
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
@@ -86,6 +86,7 @@ Rails.application.configure do
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
+    s3_host_name: 'shiba-podcast.s3.amazonaws.com'
   }
 }
 end
