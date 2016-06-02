a7dc599c2ea61613b48ec197162394e59aa12fba
shiba-git <shiba.webbusiness@gmail.com>
2016-06-02 22:06:02 +0900

    Added jPlayer

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -42,7 +42,7 @@ class EpisodesController < ApplicationController
 private
 
 	def params_episode
-		params.require(:episode).permit(:title, :description, :episode_thumbnail)
+		params.require(:episode).permit(:title, :description, :episode_thumbnail, :mp3)
 	end
 
 	def find_user
